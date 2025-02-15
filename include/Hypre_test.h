f11d4bbb48b20c69d1e09fbb78d5fe6fcfaa3d2b
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-02-15 12:21:36 +0800

    Add perprocessor

--- include/Hypre_test.h
+++ include/Hypre_test.h
@@ -1,4 +1,12 @@
+#ifndef __HYPRE_TEST_H__
+#define __HYPRE_TEST_H__
+
+
 #include "HYPRE_config.h"
 // #include "HYPRE_struct_ls.h"
 #include "HYPRE_sstruct_ls.h"
 #include "HYPRE_sstruct_mv.h"
+
+
+
+#endif // #ifndef __HYPRE_TEST_H__
