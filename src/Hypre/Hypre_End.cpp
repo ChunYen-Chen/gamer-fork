39b9ecfa4d1ba1ac05e035af832adbccac75f6eb
ChunYen-Chen <chunyenchen2019@gmail.com>
2025-02-15 12:29:16 +0800

    Print msg

--- src/Hypre/Hypre_End.cpp
+++ src/Hypre/Hypre_End.cpp
@@ -17,26 +17,30 @@
 void Hypre_End()
 {
 
+   Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
+
    int hypre_ierr;
+
 // free memory
    hypre_ierr = HYPRE_SStructGridDestroy( Hypre_grid );
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
    hypre_ierr = HYPRE_SStructGraphDestroy( Hypre_graph );
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
    hypre_ierr = HYPRE_SStructStencilDestroy( Hypre_stencil );
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
    hypre_ierr = HYPRE_SStructMatrixDestroy( Hypre_A );
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
    hypre_ierr = HYPRE_SStructVectorDestroy( Hypre_b );
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
    hypre_ierr = HYPRE_SStructVectorDestroy( Hypre_x );
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
    // HYPRE_SStructPCGDestroy( Hypre_solver );
 
 // finalize Hypre
    hypre_ierr = HYPRE_Finalize();
-         if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
+   if ( hypre_ierr ) Aux_Error( ERROR_INFO, "hypre error: %d !!\n", hypre_ierr );
 
+   Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
 
 } // FUNCTION : End_HYPRE
 #endif // #ifdef SUPPORT_HYPRE
